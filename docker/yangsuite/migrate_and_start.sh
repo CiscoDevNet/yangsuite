91d127b7d96486d95ee4483023c2ab066d84bf0b
miott <miott@cisco.com>
2021-07-01 11:18:26 -0700

    Adjust for next release.

--- docker/yangsuite/migrate_and_start.sh
+++ docker/yangsuite/migrate_and_start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Copyright 2020 Cisco Systems, Inc
-#
 # Start the yanguite service in production.
 
 # Get the system yangsuite location
@@ -12,16 +10,20 @@ cd $YS_DIR && \
   ${PY} manage.py makemigrations && \
   ${PY} manage.py migrate
 
-# Create a default super user with credentials passed on via the env or set
-# default ones and ask the user to change them
-echo "Creating default super user for admin..."
-USERNAME=${YS_ADMIN_USER:-admin}
-PASSWORD=${YS_ADMIN_PASS:-superuser}
-EMAIL=${YS_ADMIN_EMAIL:-}
+if [ -z "${YS_ADMIN_USER}" ]
+then
+    echo "No ADMIN USER set"
+fi
+if [ -z "${YS_ADMIN_PASS}" ]
+then
+    echo "No ADMIN PASSWORD set"
+fi
+if [ -z "${YS_ADMIN_EMAIL}" ]
+then
+    echo "No ADMIN EMAIL set"
+fi
 
-[ -z "${YS_ADMIN_USER}" ] && echo "No YS_ADMIN_USER set, using username: ${USERNAME}"
-[ -z "${YS_ADMIN_PASS}" ] && echo "No YS_ADMIN_PASS set, using password: ${PASSWORD} [WARNING: Please change this via the admin page]"
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('${USERNAME}', '${EMAIL}', '${PASSWORD}')" | ${PY} ${YS_DIR}/manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('${YS_ADMIN_USER}', '${YS_ADMIN_EMAIL}', '${YS_ADMIN_PASS}')" | ${PY} ${YS_DIR}/manage.py shell
 
 echo "Starting uwsgi..."
 uwsgi --ini /yangsuite/uwsgi.ini
